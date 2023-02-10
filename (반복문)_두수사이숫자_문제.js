1b692cca069ac7d7ee151cdd8433b5c51faae53a
yejin lee <ingkejin@gmail.com>
2023-02-10 15:38:30 +0900

    ⚡️ JS 반복문,조건문 문제

---
+++ (반복문)_두수사이숫자_문제.js
@@ -0,0 +1,16 @@
+/*** 두 수 사이 숫자 구하기 ***/
+
+function answer(x, y) {
+  let result = [];
+  // 코드 구현 
+  return result;
+}
+
+
+let input = [
+
+];
+for (let i = 0; i < input.length; i++) {
+  process.stdout.write(`#${i + 1} `);
+  console.log(answer(input[i][0], input[i][1]));
+}
